6f2bda9d369ddeb5dd92c80bee869bf012e6dbf4
ZSK-CRS <1625127317@qq.com>
2020-02-10 12:44:59 +0800

    Initial commit

---
+++ textlibrary/src/main/java/cn/zsk/textlibrary/FormatTextView.java
@@ -0,0 +1,26 @@
+package cn.zsk.textlibrary;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+/**
+ * Author : ZSK
+ * Date : 2020/2/10
+ * Description :
+ */
+public class FormatTextView extends AppCompatTextView {
+
+    public FormatTextView(Context context) {
+        super(context);
+    }
+
+    public FormatTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FormatTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}

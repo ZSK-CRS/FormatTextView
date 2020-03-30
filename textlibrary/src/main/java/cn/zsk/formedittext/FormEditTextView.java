95111a9da3c236591a8eecc48a77fff225c46744
ZSK-CRS <1625127317@qq.com>
2020-03-30 16:13:28 +0800

    修改bug

---
+++ textlibrary/src/main/java/cn/zsk/formedittext/FormEditTextView.java
@@ -0,0 +1,26 @@
+package cn.zsk.formedittext;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatEditText;
+
+/**
+ * @Author : ZSK
+ * @Date : 2020/3/30
+ * @Description :  表格EditTextView
+ */
+public class FormEditTextView  extends AppCompatEditText {
+
+    public FormEditTextView(Context context) {
+        super(context);
+    }
+
+    public FormEditTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FormEditTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}

bcc5eb5b2c7660555f6f58533f6415a5e82a0ce3
ZSK-CRS <1625127317@qq.com>
2020-08-18 18:38:56 +0800

    测试新工具

---
+++ commonutils/src/main/java/com/zsk/commonutils/StringUtil.java
@@ -0,0 +1,13 @@
+package com.zsk.commonutils;
+
+/**
+ * Author : ZSK
+ * Date : 2020/8/18
+ * Description :
+ */
+public class StringUtil {
+
+    public static void print(){
+        System.out.println("这世界我来了");
+    }
+}
